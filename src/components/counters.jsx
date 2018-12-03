893bb0ce4964104effc50cb5eeca92490c2d05e9
Derek Smith <dereksmith@Dereks-MacBook-Air.local>
2018-12-03 08:55:11 -0800

    added form and input

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
 import Counter from "./counter";
+// import { Link } from "react-router-dom";
 
 class Counters extends Component {
   render() {
     const { onReset, counters, onDelete, onIncrement, onMinus } = this.props;
     return (
       <div>
-        <button onClick={onReset} className="btn-primary btn-sm m-2">
-          Reset Your Order
-        </button>
+        {/* <Link
+          to="drinks/new"
+          className="btn btn-primary"
+          style={{ marginBottom: 20 }}
+        >
+          Add A New Drink
+        </Link> */}
 
         {counters.map(counter => (
           <Counter
@@ -21,6 +26,13 @@ class Counters extends Component {
             <h4>Counter #{counter.id}</h4>
           </Counter>
         ))}
+        <button onClick={onReset} className="btn-primary btn-sm m-2">
+          Reset Your Order
+        </button>
+        <button className="btn-primary btn-sm m-2">
+          <a href="./drinks" />
+          Add A New Drink
+        </button>
       </div>
     );
   }
